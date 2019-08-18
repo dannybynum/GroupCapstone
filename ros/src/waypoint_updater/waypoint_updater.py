92ff653b4a403a5568e0bc16f6e3673d1781495e
dannybynum <dannybynum@gmail.com>
2019-08-18 09:42:17 -0400

    interim hack for waypoint_updater - needs to be tested

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -80,7 +80,10 @@ class WaypointUpdater(object):
     def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
-        closest_idx = self.waypoint_tree.query([x,y], 1)[1]
+        if not None in (self.waypoint_tree):
+        	closest_idx = self.waypoint_tree.query([x,y], 1)[1]
+        else
+        	closest_idx = 0
         
         #Check if closest is ahead or behind vehicle
         closest_coord = self.waypoints_2d[closest_idx]
